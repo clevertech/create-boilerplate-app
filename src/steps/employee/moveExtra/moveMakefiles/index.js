9991d85ce806384b53ca3fcae2c2314898562054
Chad <chad@chadfurman.com>
2019-02-17 05:19:00 -0500

    98/29/127

--- src/steps/employee/moveExtra/moveMakefiles/index.js
+++ src/steps/employee/moveExtra/moveMakefiles/index.js
@@ -1,4 +1,7 @@
+import exec from '../../../../utils/exec'
 const run = async function(answers) {
+  await exec('mv boilerplate-extras/api/Makefile api/Makefile')
+  await exec('mv boilerplate-extras/frontend/Makefile frontend/Makefile')
   return answers
 }
 
--- src/steps/employee/moveExtra/moveMakefiles/index.test.js
+++ src/steps/employee/moveExtra/moveMakefiles/index.test.js
@@ -1,3 +1,22 @@
+import exec from '../../../../utils/exec'
+
+import subject from './'
+
+jest.mock('../../../../utils/exec')
+const fakeAnswers = { a: 123 }
 describe('Move makefiles', () => {
-  test.todo('moves extra makefiles into place')
+  it('moves api makefile into place', async () => {
+    const answers = await subject(fakeAnswers)
+    expect(exec).toHaveBeenCalledWith(
+      'mv boilerplate-extras/api/Makefile api/Makefile'
+    )
+    expect(answers).toEqual(expect.objectContaining(fakeAnswers))
+  })
+  it('moves frontend makefile into place', async () => {
+    const answers = await subject(fakeAnswers)
+    expect(exec).toHaveBeenCalledWith(
+      'mv boilerplate-extras/frontend/Makefile frontend/Makefile'
+    )
+    expect(answers).toEqual(expect.objectContaining(fakeAnswers))
+  })
 })
