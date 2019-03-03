821cbf67ce81ef2af8a2e67a3e0c9b07ab5c345e
Chad <chad@chadfurman.com>
2019-03-02 23:50:43 -0500

    improved adminSuffix test

--- src/steps/admin/adminSuffix/index.test.js
+++ src/steps/admin/adminSuffix/index.test.js
@@ -1,8 +1,10 @@
 import subject from '.'
+global.console = { log: jest.fn() }
 describe('admin suffix', () => {
   it('returns answers when called', async () => {
     const fakeAnswers = { asdf: 12 }
     const answers = await subject(fakeAnswers)
+    expect(global.console.log).toHaveBeenCalled()
     expect(answers).toBe(fakeAnswers)
   })
 })
