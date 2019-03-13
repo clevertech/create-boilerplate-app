6f2960c07b391f897c176f7507a185817ffe7103
gamebak <gamebak>
2019-03-13 22:32:42 +0200

    added main test in configureHelmFrontend, test is failing

---
+++ src/steps/admin/configureHelm/configureFrontendHelm/index.test.js
@@ -0,0 +1,33 @@
+import subject from './'
+import setImage from './setImageRepository/index'
+import setSubdomain from './setSubdomain/index'
+import generateRandom from '../../../../utils/generateRandom'
+
+jest.mock('./setSubdomain/index')
+const fakeSlug = generateRandom(6)
+const fakeAnswers = { base: { prompt: { admin: true, projectSlug: fakeSlug } } }
+
+describe('Generate Frontend Admin Helm script', () => {
+  afterEach(jest.clearAllMocks)
+  it('generate random subdomain', async () => {
+    // const answers = await subject(fakeAnswers)
+    expect(setSubdomain).toHaveBeenCalledWith(fakeSlug)
+    // expect(answers).toEqual(expect.objectContaining(fakeAnswers))
+  })
+  test.todo('generates frontend helm')
+  // const generateHelmFrontend = async (answers, randomValue) => {
+  //   const helmFile = path.join(__dirname, 'helm/frontend.yml')
+  //   const helmFileSource = await fs.readFile(helmFile, 'utf8')
+  //   const helm = yaml.parse(helmFileSource)
+  //   helm.deployment.image.repository = helm.deployment.image.repository.replace(
+  //     /boilerplate/g,
+  //     dashify(answers.projectName)
+  //   )
+  //   helm.ingress.hosts[0].rules[0].subdomain = helm.ingress.hosts[0].rules[0].subdomain
+  //     .replace(/boilerplate/g, dashify(answers.projectName))
+  //     .replace(/randomvalue/g, randomValue)
+  //
+  //   const destFile = path.join(basedir, 'helm-frontend-development.yml')
+  //   fs.writeFile(destFile, yaml.stringify(helm, 4, 2))
+  // }
+})
--- src/steps/admin/configureHelm/configureFrontendHelm/setSubdomain/index.test.js
+++ src/steps/admin/configureHelm/configureFrontendHelm/setSubdomain/index.test.js
@@ -1,8 +1,9 @@
 import subject from './'
 import replaceStringInFile from '../../../../../utils/replaceStringInFile'
+import generateRandom from '../../../../../utils/generateRandom'
 
 jest.mock('../../../../../utils/replaceStringInFile')
-const fakeSlug = 'new-subdomain'
+const fakeSlug = generateRandom(6)
 const fakeAnswers = { base: { prompt: { admin: true, projectSlug: fakeSlug } } }
 
 describe('Generate Frontend Admin Helm script', () => {
