9e174a867786728d6eed57b28fbd8f7d24af9121
Chad <chad@chadfurman.com>
2019-03-16 20:09:30 -0400

    Using yaml utility instead of replaceStringInFile to target specific fields

--- src/steps/admin/configureHelm/configureFrontendHelm/setImageRepository/index.js
+++ src/steps/admin/configureHelm/configureFrontendHelm/setImageRepository/index.js
@@ -1,11 +1,29 @@
-import replaceStringInFile from '../../../../../utils/replaceStringInFile'
+import yaml from '../../../../../utils/yaml'
+import { pathOr } from 'ramda'
+
+const filePath = __dirname + '/../../../../../helm/frontend.yml'
 
 const run = async function(answers) {
-  await replaceStringInFile(
-    __dirname + '/../../../../../helm/frontend.yml',
-    'boilerplateRepo',
+  const fileContents = await yaml(filePath)
+  let imageRepo = pathOr(
+    null,
+    ['deployment', 'image', 'repository'],
+    fileContents
+  )
+
+  if (!imageRepo) {
+    console.warn(
+      'admin/configureHelm/configureFrontendHelm skipped: imageRepo missing.'
+    )
+    return answers
+  }
+
+  fileContents.deployment.image.repository = imageRepo.replace(
+    /boilerplate/g,
     answers.base.prompt.projectSlug
   )
+
+  await yaml(filePath, fileContents)
   return answers
 }
 
--- src/steps/admin/configureHelm/configureFrontendHelm/setImageRepository/index.test.js
+++ src/steps/admin/configureHelm/configureFrontendHelm/setImageRepository/index.test.js
@@ -1,18 +1,35 @@
 import subject from './'
-import replaceStringInFile from '../../../../../utils/replaceStringInFile'
+import yaml from '../../../../../utils/yaml'
 
-jest.mock('../../../../../utils/replaceStringInFile')
+function mockHelmDeploymentImageRepoObject(repo) {
+  return {
+    deployment: {
+      image: {
+        repository: repo
+      }
+    }
+  }
+}
+
+jest.mock('../../../../../utils/yaml', () =>
+  jest.fn(() => mockHelmDeploymentImageRepoObject('boilerplate'))
+)
 const fakeSlug = 'image-repository'
 const fakeAnswers = { base: { prompt: { admin: true, projectSlug: fakeSlug } } }
+const filePath = __dirname + '/../../../../../helm/frontend.yml'
 
 describe('Generate Frontend Admin Helm script', () => {
   afterEach(jest.clearAllMocks)
+
   it('configures image repository', async () => {
     const answers = await subject(fakeAnswers)
-    expect(replaceStringInFile).toHaveBeenCalledWith(
-      __dirname + '/../../../../../helm/frontend.yml',
-      'boilerplateRepo',
-      fakeSlug
+    expect(yaml).not.toHaveBeenCalledWith(
+      filePath,
+      mockHelmDeploymentImageRepoObject('boilerplate')
+    )
+    expect(yaml).toHaveBeenCalledWith(
+      filePath,
+      mockHelmDeploymentImageRepoObject(fakeSlug)
     )
     expect(answers).toEqual(expect.objectContaining(fakeAnswers))
   })
