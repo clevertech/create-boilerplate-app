37a9243bae659de9c0a27d3040d6b5b044259d89
Chad <chad@chadfurman.com>
2019-03-18 01:55:02 -0400

    Fix step/start/index test

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,11 @@
 import start from './steps/start'
+import subject from './'
 
 jest.mock('./steps/start')
 
 describe('entrypoint', () => {
   it('calls start when imported', () => {
+    subject()
     expect(start).toHaveBeenCalled()
   })
 })
