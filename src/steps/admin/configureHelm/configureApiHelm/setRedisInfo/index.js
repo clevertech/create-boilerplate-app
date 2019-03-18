5f373e87a911e14126945a5799b00a648e00c2fa
Chad <chad@chadfurman.com>
2019-03-17 22:06:43 -0400

    implement setRedisInfo submodule

--- src/steps/admin/configureHelm/configureApiHelm/setRedisInfo/index.js
+++ src/steps/admin/configureHelm/configureApiHelm/setRedisInfo/index.js
@@ -1,17 +1,30 @@
+import yaml from '../../../../../utils/yaml'
+import config from 'config'
+import { mergeDeepRight } from 'ramda'
+
+const filePath = './helm/api.yml'
+
 const run = async function(answers) {
-  // const redisInfo = {
-  //   helm: {
-  //     secrets: [
-  //       {
-  //         data: {
-  //           REDIS_HOST: answers.redishost,
-  //           REDIS_PORT: '6379',
-  //           REDIS_PREFIX: dbName + '_development'
-  //         }
-  //       }
-  //     ]
-  //   }
-  // }
+  const fileContents = await yaml(filePath)
+
+  const redisHost = answers.admin.prompt.redisHost
+  const redisPort = config.redis.port
+  const redisPrefix = answers.base.prompt.projectSlug
+
+  const dbInfo = {
+    secrets: [
+      {
+        data: {
+          REDIS_HOST: redisHost,
+          REDIS_PORT: redisPort,
+          REDIS_PREFIX: redisPrefix + '_development'
+        }
+      }
+    ]
+  }
+
+  await yaml(filePath, mergeDeepRight(fileContents, dbInfo))
+
   return answers
 }
 
--- src/steps/admin/configureHelm/configureApiHelm/setRedisInfo/index.test.js
+++ src/steps/admin/configureHelm/configureApiHelm/setRedisInfo/index.test.js
@@ -1,32 +1,36 @@
 import subject from './'
 import yaml from '../../../../../utils/yaml'
+import config from 'config'
 
 const mockYaml = {}
 jest.mock('../../../../../utils/yaml', () => jest.fn(() => mockYaml))
 
-const fakeSlug = 'project_slug'
-const fakeAnswers = { base: { prompt: { admin: true, projectSlug: fakeSlug } } }
+const mockSlug = 'project_slug'
+const mockRedisHost = 'mockredishost'
+const fakeAnswers = {
+  base: { prompt: { admin: true, projectSlug: mockSlug } },
+  admin: { prompt: { redisHost: mockRedisHost } }
+}
 
 describe('Admin ConfigureApiHelm: setRedisInfo', () => {
   afterEach(jest.clearAllMocks)
   it('configures redis secrets', async () => {
     const answers = await subject(fakeAnswers)
+    const redisHost = fakeAnswers.admin.prompt.redisHost
+    const redisPort = config.redis.port
+    const redisPrefix = fakeAnswers.base.prompt.projectSlug
     expect(yaml).toHaveBeenCalledWith(
-      expect.stringMatching(/helm-api-development.yml/),
+      expect.stringMatching(/\.\/helm\/api\.yml/),
       expect.objectContaining({
-        helm: expect.objectContaining({
-          secrets: expect.arrayContaining([
-            expect.objectContaining({
-              data: expect.objectContaining({
-                //     REDIS_HOST: answers.redishost,
-                //     REDIS_PORT: '6379',
-                //     REDIS_PREFIX: dbName + '_development',
-                //     SESSION_SECRET: nanoid(),
-                //     HEALTH_CHECK_SECRET: nanoid()
-              })
+        secrets: expect.arrayContaining([
+          expect.objectContaining({
+            data: expect.objectContaining({
+              REDIS_HOST: redisHost,
+              REDIS_PORT: redisPort,
+              REDIS_PREFIX: redisPrefix + '_development'
             })
-          ])
-        })
+          })
+        ])
       })
     )
     expect(answers).toEqual(expect.objectContaining(fakeAnswers))
