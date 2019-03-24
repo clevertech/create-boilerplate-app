8c58c3816799549f0432a0af78aed51b35e0d2a8
Chad <chad@chadfurman.com>
2019-03-24 17:17:44 -0400

    actually, it should clone into 'boilerplate' ...

--- src/steps/base/cloneBase/index.js
+++ src/steps/base/cloneBase/index.js
@@ -2,7 +2,7 @@ import clone from '../../../utils/clone'
 import config from '../../../../config.json'
 
 const run = async function(answers) {
-  await clone(config.boilerplateRepo, answers.dirName)
+  await clone(config.boilerplateRepo, './')
   return answers
 }
 
