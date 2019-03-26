aac49447c0006550d0f57813f1ed2390716b7419
gamebak <gamebak>
2019-03-26 16:41:38 +0200

    fix stackStepMove and add extra logs

--- src/steps/stack/stackStepDelete/index.js
+++ src/steps/stack/stackStepDelete/index.js
@@ -4,7 +4,6 @@ const run = async function(answers) {
   console.log('Stack: delete')
   console.log('rm -rf ' + answers.baseDir + 'stacks')
   await exec('rm -rf ' + answers.baseDir + 'stacks')
-  console.log('Stack delete: finished')
   return answers
 }
 
