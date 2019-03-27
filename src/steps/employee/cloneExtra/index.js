b9177b543f04f0a6e67be61a56230d92f74207c9
gamebak <gamebak>
2019-03-27 16:09:01 +0200

    fix cloneExtra for employee and add console log

--- src/steps/employee/cloneExtra/index.js
+++ src/steps/employee/cloneExtra/index.js
@@ -4,7 +4,7 @@ import config from '../../../../config.json'
 export const EXTRA_REPO = config.boilerplateExtrasRepo
 const run = async function(answers) {
   console.info('Employee: clone')
-  clone(EXTRA_REPO, answers.baseDir)
+  await clone(EXTRA_REPO, answers.baseDir)
   return answers
 }
 
--- src/steps/employee/moveExtra/index.js
+++ src/steps/employee/moveExtra/index.js
@@ -3,6 +3,7 @@ import moveMakefiles from './moveMakefiles'
 import moveBuildspec from './moveBuildspecs'
 
 const run = async function(answers) {
+  console.info('Employee: move')
   answers = await moveTerraform(answers)
   answers = await moveMakefiles(answers)
   answers = await moveBuildspec(answers)
